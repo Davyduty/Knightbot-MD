e80f20b7cc112003cedbf5a423efa491c15771a9
Davyduty <129392472+Davyduty@users.noreply.github.com>
2025-04-14 20:20:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Davyduty_MD',
+  author: 'Enjoyment Minister',
+  botName: "Davyduty_Bot",
+  botOwner: 'Davyduty', // Your name
+  ownerNumber: '254717442167', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
